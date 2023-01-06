03637a6a9a243180075244e2fb460d4f5665b866
Kirill Fomichev <fanatid@ya.ru>
2023-01-05 22:11:52 -0300

    Add check tool for plugin config

---
+++ src/bin/config-check.rs
@@ -0,0 +1,16 @@
+use {clap::Parser, solana_geyser_grpc::config::Config};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long, default_value_t = String::from("config.json"))]
+    /// Path to config
+    config: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let _config = Config::load_from_file(args.config)?;
+    println!("Config is OK!");
+    Ok(())
+}
