0ffb9c5a73279361d04ffaaa555c17174ab7b951
Kirill Fomichev <fanatid@ya.ru>
2023-03-17 00:59:54 -0300

    add rust client lib (#91)
    Thanks for the pr.
    The WebSocket proxy also needs it.

---
+++ rust-client/src/bin/client.rs
@@ -0,0 +1,196 @@
+use {
+    backoff::{future::retry, ExponentialBackoff},
+    clap::Parser,
+    futures::{sink::SinkExt, stream::StreamExt},
+    log::{error, info},
+    solana_geyser_grpc::proto::{
+        SubscribeRequest, SubscribeRequestFilterAccounts, SubscribeRequestFilterBlocks,
+        SubscribeRequestFilterBlocksMeta, SubscribeRequestFilterSlots,
+        SubscribeRequestFilterTransactions,
+    },
+    solana_geyser_grpc_client::{GeyserGrpcClient, GeyserGrpcClientError},
+    std::collections::HashMap,
+};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long, default_value_t = String::from("http://127.0.0.1:10000"))]
+    /// Service endpoint
+    endpoint: String,
+
+    #[clap(long)]
+    x_token: Option<String>,
+
+    /// Subscribe on accounts updates
+    #[clap(long)]
+    accounts: bool,
+
+    /// Filter by Account Pubkey
+    #[clap(long)]
+    accounts_account: Vec<String>,
+
+    /// Filter by Owner Pubkey
+    #[clap(long)]
+    accounts_owner: Vec<String>,
+
+    /// Subscribe on slots updates
+    #[clap(long)]
+    slots: bool,
+
+    /// Subscribe on transactions updates
+    #[clap(long)]
+    transactions: bool,
+
+    /// Filter vote transactions
+    #[clap(long)]
+    transactions_vote: Option<bool>,
+
+    /// Filter failed transactions
+    #[clap(long)]
+    transactions_failed: Option<bool>,
+
+    /// Filter by transaction signature
+    #[clap(long)]
+    transactions_signature: Option<String>,
+
+    /// Filter included account in transactions
+    #[clap(long)]
+    transactions_account_include: Vec<String>,
+
+    /// Filter excluded account in transactions
+    #[clap(long)]
+    transactions_account_exclude: Vec<String>,
+
+    /// Subscribe on block updates
+    #[clap(long)]
+    blocks: bool,
+
+    /// Subscribe on block meta updates (without transactions)
+    #[clap(long)]
+    blocks_meta: bool,
+
+    // Resubscribe (only to slots) after
+    #[clap(long)]
+    resub: Option<u16>,
+}
+
+type SlotsFilterMap = HashMap<String, SubscribeRequestFilterSlots>;
+type AccountFilterMap = HashMap<String, SubscribeRequestFilterAccounts>;
+type TransactionsFilterMap = HashMap<String, SubscribeRequestFilterTransactions>;
+type BlocksFilterMap = HashMap<String, SubscribeRequestFilterBlocks>;
+type BlocksMetaFilterMap = HashMap<String, SubscribeRequestFilterBlocksMeta>;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let args = Args::parse();
+
+    let mut accounts: AccountFilterMap = HashMap::new();
+    if args.accounts {
+        accounts.insert(
+            "client".to_owned(),
+            SubscribeRequestFilterAccounts {
+                account: args.accounts_account,
+                owner: args.accounts_owner,
+            },
+        );
+    }
+
+    let mut slots: SlotsFilterMap = HashMap::new();
+    if args.slots {
+        slots.insert("client".to_owned(), SubscribeRequestFilterSlots {});
+    }
+
+    let mut transactions: TransactionsFilterMap = HashMap::new();
+    if args.transactions {
+        transactions.insert(
+            "client".to_string(),
+            SubscribeRequestFilterTransactions {
+                vote: args.transactions_vote,
+                failed: args.transactions_failed,
+                signature: args.transactions_signature,
+                account_include: args.transactions_account_include,
+                account_exclude: args.transactions_account_exclude,
+            },
+        );
+    }
+
+    let mut blocks: BlocksFilterMap = HashMap::new();
+    if args.blocks {
+        blocks.insert("client".to_owned(), SubscribeRequestFilterBlocks {});
+    }
+
+    let mut blocks_meta: BlocksMetaFilterMap = HashMap::new();
+    if args.blocks_meta {
+        blocks_meta.insert("client".to_owned(), SubscribeRequestFilterBlocksMeta {});
+    }
+
+    let resub: u16 = args.resub.unwrap_or(0);
+
+    // The default exponential backoff strategy intervals:
+    // [500ms, 750ms, 1.125s, 1.6875s, 2.53125s, 3.796875s, 5.6953125s,
+    // 8.5s, 12.8s, 19.2s, 28.8s, 43.2s, 64.8s, 97s, ... ]
+    retry(ExponentialBackoff::default(), move || {
+        let (endpoint, x_token) = (args.endpoint.clone(), args.x_token.clone());
+        let (slots, accounts, transactions, blocks, blocks_meta) = (
+            slots.clone(),
+            accounts.clone(),
+            transactions.clone(),
+            blocks.clone(),
+            blocks_meta.clone(),
+        );
+
+        async move {
+            info!("Retry to connect to the server");
+            if let Err(err) = GeyserGrpcClient::connect(endpoint.clone(), x_token.clone()) {
+                error!("{:?}", err);
+            }
+            let mut client = GeyserGrpcClient::connect(endpoint, x_token)?;
+            let (mut subscribe_tx, mut stream) = client.subscribe().await?;
+            subscribe_tx
+                .send(SubscribeRequest {
+                    slots,
+                    accounts,
+                    transactions,
+                    blocks,
+                    blocks_meta,
+                })
+                .await
+                .map_err(GeyserGrpcClientError::SubscribeSendError)?;
+
+            info!("stream opened");
+            let mut counter = 0;
+            while let Some(message) = stream.next().await {
+                match message {
+                    Ok(message) => info!("new message: {:?}", message),
+                    Err(error) => error!("error: {:?}", error),
+                }
+
+                // Example to illustrate how to resubscribe/update the subscription
+                counter += 1;
+                if counter == resub {
+                    let mut new_slots: SlotsFilterMap = HashMap::new();
+                    new_slots.insert("client".to_owned(), SubscribeRequestFilterSlots {});
+
+                    subscribe_tx
+                        .send(SubscribeRequest {
+                            slots: new_slots.clone(),
+                            accounts: HashMap::default(),
+                            transactions: HashMap::default(),
+                            blocks: HashMap::default(),
+                            blocks_meta: HashMap::default(),
+                        })
+                        .await
+                        .map_err(GeyserGrpcClientError::SubscribeSendError)?;
+                }
+            }
+            info!("stream closed");
+            Ok(())
+        }
+    })
+    .await
+    .map_err(Into::into)
+}
---
+++ rust/src/lib.rs
@@ -0,0 +1,165 @@
+use {
+    bytes::Bytes,
+    futures::{
+        channel::mpsc,
+        sink::{Sink, SinkExt},
+        stream::Stream,
+    },
+    http::uri::InvalidUri,
+    solana_geyser_grpc::proto::{
+        geyser_client::GeyserClient, SubscribeRequest, SubscribeRequestFilterAccounts,
+        SubscribeRequestFilterBlocks, SubscribeRequestFilterBlocksMeta,
+        SubscribeRequestFilterSlots, SubscribeRequestFilterTransactions, SubscribeUpdate,
+    },
+    std::collections::HashMap,
+    tonic::{
+        codec::Streaming,
+        metadata::{errors::InvalidMetadataValue, AsciiMetadataValue},
+        service::{interceptor::InterceptedService, Interceptor},
+        transport::channel::{Channel, ClientTlsConfig},
+        Request, Response, Status,
+    },
+};
+
+pub const XTOKEN_LENGTH: usize = 28;
+
+#[derive(Debug, thiserror::Error)]
+pub enum GeyserGrpcClientError {
+    #[error("Invalid URI: {0}")]
+    InvalidUri(#[from] InvalidUri),
+    #[error("Failed to parse x-token: {0}")]
+    MetadataValueError(#[from] InvalidMetadataValue),
+    #[error("Invalid X-Token length: {0}, expected 28")]
+    InvalidXTokenLength(usize),
+    #[error("gRPC transport error: {0}")]
+    TonicError(#[from] tonic::transport::Error),
+    #[error("gRPC status: {0}")]
+    TonicStatus(#[from] Status),
+    #[error("Failed to send subscribe request: {0}")]
+    SubscribeSendError(#[from] mpsc::SendError),
+}
+
+pub type GeyserGrpcClientResult<T> = Result<T, GeyserGrpcClientError>;
+
+pub struct GeyserGrpcClient<F> {
+    client: GeyserClient<InterceptedService<Channel, F>>,
+}
+
+impl GeyserGrpcClient<()> {
+    pub fn connect<E, T>(
+        endpoint: E,
+        x_token: Option<T>,
+    ) -> GeyserGrpcClientResult<GeyserGrpcClient<impl Interceptor>>
+    where
+        E: Into<Bytes>,
+        T: TryInto<AsciiMetadataValue, Error = InvalidMetadataValue>,
+    {
+        let mut endpoint = Channel::from_shared(endpoint)?;
+        if endpoint.uri().scheme_str() == Some("https") {
+            endpoint = endpoint.tls_config(ClientTlsConfig::new())?;
+        }
+        let channel = endpoint.connect_lazy();
+
+        let x_token: Option<AsciiMetadataValue> = match x_token {
+            Some(x_token) => Some(x_token.try_into()?),
+            None => None,
+        };
+        match x_token {
+            Some(token) if token.len() != XTOKEN_LENGTH => {
+                return Err(GeyserGrpcClientError::InvalidXTokenLength(token.len()));
+            }
+            _ => {}
+        }
+
+        let client = GeyserClient::with_interceptor(channel, move |mut req: Request<()>| {
+            if let Some(x_token) = x_token.clone() {
+                req.metadata_mut().insert("x-token", x_token);
+            }
+            Ok(req)
+        });
+
+        Ok(GeyserGrpcClient { client })
+    }
+}
+
+impl<F: Interceptor> GeyserGrpcClient<F> {
+    pub async fn subscribe(
+        &mut self,
+    ) -> GeyserGrpcClientResult<(
+        impl Sink<SubscribeRequest, Error = mpsc::SendError>,
+        impl Stream<Item = Result<SubscribeUpdate, Status>>,
+    )> {
+        let (subscribe_tx, subscribe_rx) = mpsc::unbounded();
+        let response: Response<Streaming<SubscribeUpdate>> =
+            self.client.subscribe(subscribe_rx).await?;
+        Ok((subscribe_tx, response.into_inner()))
+    }
+
+    pub async fn subscribe_once(
+        &mut self,
+        slots: HashMap<String, SubscribeRequestFilterSlots>,
+        accounts: HashMap<String, SubscribeRequestFilterAccounts>,
+        transactions: HashMap<String, SubscribeRequestFilterTransactions>,
+        blocks: HashMap<String, SubscribeRequestFilterBlocks>,
+        blocks_meta: HashMap<String, SubscribeRequestFilterBlocksMeta>,
+    ) -> GeyserGrpcClientResult<impl Stream<Item = Result<SubscribeUpdate, Status>>> {
+        let (mut subscribe_tx, response) = self.subscribe().await?;
+        subscribe_tx
+            .send(SubscribeRequest {
+                slots,
+                accounts,
+                transactions,
+                blocks,
+                blocks_meta,
+            })
+            .await?;
+        Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{GeyserGrpcClient, GeyserGrpcClientError};
+
+    #[tokio::test]
+    async fn test_channel_https_success() {
+        let endpoint = "https://ams17.rpcpool.com:443";
+        let x_token = "1000000000000000000000000007";
+        let res = GeyserGrpcClient::connect(endpoint, Some(x_token));
+        assert!(res.is_ok())
+    }
+
+    #[tokio::test]
+    async fn test_channel_http_success() {
+        let endpoint = "http://127.0.0.1:10000";
+        let x_token = "1234567891012141618202224268";
+        let res = GeyserGrpcClient::connect(endpoint, Some(x_token));
+        assert!(res.is_ok())
+    }
+
+    #[tokio::test]
+    async fn test_channel_invalid_token_some() {
+        let endpoint = "http://127.0.0.1:10000";
+        let x_token = "123";
+        let res = GeyserGrpcClient::connect(endpoint, Some(x_token));
+        assert!(matches!(
+            res,
+            Err(GeyserGrpcClientError::InvalidXTokenLength(_))
+        ));
+    }
+
+    #[tokio::test]
+    async fn test_channel_invalid_token_none() {
+        let endpoint = "http://127.0.0.1:10000";
+        let res = GeyserGrpcClient::connect::<_, String>(endpoint, None);
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_channel_invalid_uri() {
+        let endpoint = "sites/files/images/picture.png";
+        let x_token = "1234567891012141618202224268";
+        let res = GeyserGrpcClient::connect(endpoint, Some(x_token));
+        assert!(matches!(res, Err(GeyserGrpcClientError::InvalidUri(_))));
+    }
+}
