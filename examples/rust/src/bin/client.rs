c89f7411e65264ee0b2623a1d815ceb340a924bf
Kirill Fomichev <fanatid@ya.ru>
2023-06-29 16:56:57 -0400

    geyser: add TokenAccountState to accounts filter

--- examples/rust/src/bin/client.rs
+++ examples/rust/src/bin/client.rs
@@ -110,6 +110,10 @@ struct ActionSubscribe {
     #[clap(long)]
     accounts_datasize: Option<u64>,
 
+    /// Filter valid token accounts
+    #[clap(long)]
+    accounts_token_account_state: bool,
+
     /// Receive only part of updated data account, format: `offset,size`
     #[clap(long)]
     accounts_data_slice: Vec<String>,
@@ -191,6 +195,11 @@ impl Action {
                     if let Some(datasize) = args.accounts_datasize {
                         filters.push(SubscribeRequestFilterAccountsFilter {
                             filter: Some(AccountsFilterDataOneof::Datasize(datasize)),
+                        });
+                    }
+                    if args.accounts_token_account_state {
+                        filters.push(SubscribeRequestFilterAccountsFilter {
+                            filter: Some(AccountsFilterDataOneof::TokenAccountState(true)),
                         });
                     }
 
