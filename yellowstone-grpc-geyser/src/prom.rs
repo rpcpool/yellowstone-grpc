6b06d64a57242d217adf6bc0e6f544f0a0d8c00d
Kirill Fomichev <fanatid@ya.ru>
2023-08-15 11:17:08 -0400

    geyser: add package name to version info (#173)

--- yellowstone-grpc-geyser/src/prom.rs
+++ yellowstone-grpc-geyser/src/prom.rs
@@ -18,7 +18,7 @@ lazy_static::lazy_static! {
 
     static ref VERSION: IntCounterVec = IntCounterVec::new(
         Opts::new("version", "Plugin version info"),
-        &["buildts", "git", "proto", "rustc", "solana", "version"]
+        &["buildts", "git", "package", "proto", "rustc", "solana", "version"]
     ).unwrap();
 
     pub static ref SLOT_STATUS: IntGaugeVec = IntGaugeVec::new(
@@ -65,6 +65,7 @@ impl PrometheusService {
                 .with_label_values(&[
                     VERSION_INFO.buildts,
                     VERSION_INFO.git,
+                    VERSION_INFO.package,
                     VERSION_INFO.proto,
                     VERSION_INFO.rustc,
                     VERSION_INFO.solana,
