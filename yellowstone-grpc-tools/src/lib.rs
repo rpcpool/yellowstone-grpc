77fe34b260fec09a78550e9b76069660437661ea
Kirill Fomichev <fanatid@ya.ru>
2023-10-19 18:39:33 +0400

    tools: add Google Pub/Sub (#211)
    Signed-off-by: Kirill Fomichev <fanatid@ya.ru>
    (cherry picked from commit d8f4f38258c02373fe3061bf0d4c8d1fb7b0bb5f)

--- yellowstone-grpc-tools/src/lib.rs
+++ yellowstone-grpc-tools/src/lib.rs
@@ -2,6 +2,25 @@
 #![deny(clippy::missing_const_for_fn)]
 #![deny(clippy::trivially_copy_pass_by_ref)]
 
+pub mod config;
+pub mod google_pubsub;
 pub mod kafka;
 pub mod prom;
 pub mod version;
+
+use {
+    futures::future::{BoxFuture, FutureExt},
+    tokio::signal::unix::{signal, SignalKind},
+};
+
+pub fn create_shutdown() -> anyhow::Result<BoxFuture<'static, ()>> {
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+    Ok(async move {
+        tokio::select! {
+            _ = sigint.recv() => {},
+            _ = sigterm.recv() => {}
+        };
+    }
+    .boxed())
+}
