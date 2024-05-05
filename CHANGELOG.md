6c21de5995d4bdd1afea66d9b00a2b9685b467ed
Kirill Fomichev <fanatid@ya.ru>
2024-05-04 20:24:57 -0400

    geyser: use runtime instaed of unconstrained

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,8 @@ The minor version will be incremented upon a breaking change and the patch versi
 
 ### Features
 
+- geyser: use runtime instaed of unconstrained ([#331](https://github.com/rpcpool/yellowstone-grpc/pull/331))
+
 ### Breaking
 
 ## 2024-04-30
--- yellowstone-grpc-geyser/src/lib.rs
+++ yellowstone-grpc-geyser/src/lib.rs
@@ -8,3 +8,11 @@ pub mod grpc;
 pub mod plugin;
 pub mod prom;
 pub mod version;
+
+pub fn get_thread_name() -> String {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+    let id = ATOMIC_ID.fetch_add(1, Ordering::Relaxed);
+    format!("solGeyserGrpc{id:02}")
+}
