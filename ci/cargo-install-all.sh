c4e332cd0b7ec0fa46adbb870c59ad26c9ec8494
Kirill Fomichev <fanatid@ya.ru>
2024-07-12 13:57:56 -0500

    remove scylladb feature

--- ci/cargo-install-all.sh
+++ ci/cargo-install-all.sh
@@ -69,7 +69,7 @@ mkdir -p "$installDir/lib"
 (
   set -x
   # shellcheck disable=SC2086 # Don't want to double quote $rust_version
-  cargo $maybeRustVersion build $maybeReleaseFlag --features scylladb
+  cargo $maybeRustVersion build $maybeReleaseFlag
 )
 
 cp -fv "target/$buildVariant/${GEYSER_PLUGIN_LIB}.$libExt" "$installDir"/lib/
