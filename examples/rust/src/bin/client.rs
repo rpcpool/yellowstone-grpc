fd1e5dd37bb0346f353c6f7522b184aa103c1bd3
Pierre <Arrowana@users.noreply.github.com>
2025-08-08 23:15:07 +1000

    fix: client example default to filter default (None, false...) rather than a given value (#604)

--- examples/rust/src/bin/client.rs
+++ examples/rust/src/bin/client.rs
@@ -252,6 +252,7 @@ struct ActionSubscribe {
     accounts: bool,
 
     /// Filter by presence of field txn_signature
+    #[clap(long)]
     accounts_nonempty_txn_signature: Option<bool>,
 
     /// Filter by Account Pubkey
@@ -292,11 +293,11 @@ struct ActionSubscribe {
 
     /// Filter slots by commitment
     #[clap(long)]
-    slots_filter_by_commitment: bool,
+    slots_filter_by_commitment: Option<bool>,
 
     /// Subscribe on interslot slot updates
     #[clap(long)]
-    slots_interslot_updates: bool,
+    slots_interslot_updates: Option<bool>,
 
     /// Subscribe on transactions updates
     #[clap(long)]
@@ -479,10 +480,10 @@ impl Action {
                     accounts.insert(
                         "client".to_owned(),
                         SubscribeRequestFilterAccounts {
-                            nonempty_txn_signature: args.accounts_nonempty_txn_signature,
                             account: accounts_account,
                             owner: args.accounts_owner.clone(),
                             filters,
+                            nonempty_txn_signature: args.accounts_nonempty_txn_signature,
                         },
                     );
                 }
@@ -492,8 +493,8 @@ impl Action {
                     slots.insert(
                         "client".to_owned(),
                         SubscribeRequestFilterSlots {
-                            filter_by_commitment: Some(args.slots_filter_by_commitment),
-                            interslot_updates: Some(args.slots_interslot_updates),
+                            filter_by_commitment: args.slots_filter_by_commitment,
+                            interslot_updates: args.slots_interslot_updates,
                         },
                     );
                 }
